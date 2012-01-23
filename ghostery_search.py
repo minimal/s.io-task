1ffd5fc061bd5e4f1bf9de26001f3731dddc121e
Chris McDevitt <christophermcdevitt@gmail.com>
2012-01-23 19:09:43 +0000

    Use requests lib with async get
    * do chunks of 100 requests at a time

--- ghostery_search.py
+++ ghostery_search.py
@@ -7,11 +7,10 @@ import urllib2
 import json
 import re
 
+from requests import async
 
-def check_site(domain):
-    url = "http://" + domain
-    response = urllib2.urlopen(url, timeout=2)
-    html = response.read()
+
+def check_for_scripts(html):
     scripts = []
     for bug in bugs:
         if bug['regex']:
@@ -19,6 +18,34 @@ def check_site(domain):
             if match:
                 scripts.append(bug['id'])
     return scripts
+
+def check_site(domain):
+    url = "http://" + domain
+    response = urllib2.urlopen(url, timeout=2)
+    html = response.read()
+    
+    return check_for_scripts(html)
+
+nsites = 1
+def check_response(response):
+    global nsites
+    try:
+        scripts = check_for_scripts(response.text)
+        print nsites, response.url, scripts
+        nsites += 1
+    except Exception, e:
+        print e
+        # print  "error"    
+    
+
+def check_sites_async(domains):
+    "Get html of sites usign requests.async"
+    rs = (async.get("http://" + domain, hooks=(dict(response=check_response)),
+                    timeout=1.0, prefetch=False)
+            for domain in domains)
+    
+    async.map(rs, size=100)
+
 
 def import_bugs(bugsfile="bugs.json"):
     with open(bugsfile) as bf:
@@ -28,7 +55,6 @@ def import_bugs(bugsfile="bugs.json"):
 def compile_regexes(bugs):
     errs = 0
     for bug in bugs:
-        #print "compile", bug['pattern']
         try:
             bug['regex'] = re.compile(bug['pattern'])
         except Exception as exc:
@@ -49,14 +75,20 @@ def import_sites(filename):
 if __name__ == '__main__':
     global bugs
     bugs = compile_regexes(import_bugs())
+    #sites = import_sites("top100.txt")
     sites = import_sites("100k.txt")
-    for site in sites:
-        try:
-            scripts = check_site(site)
-            print site, scripts
-        except Exception, e:
-            print e
-            print site, "error"
+    import random
+    # shuffle list while testing to not spam the same sites all the time
+    sitelist = list(sites)
+    random.shuffle(sitelist)
+    check_sites_async(sitelist)
+    # for site in sites:
+    #     try:
+    #         scripts = check_site(site)
+    #         print site, scripts
+    #     except Exception, e:
+    #         print e
+    #         print site, "error"
         
         
 
